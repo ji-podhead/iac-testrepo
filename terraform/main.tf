c33171157d36f0f004f116ff5c6f62d0b785132a
J.I. /podhead <117015142+ji-podhead@users.noreply.github.com>
2024-07-17 15:47:34 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,15 +7,6 @@
       }
 }
 }
-  locals {
-  // conf = data.external.config.result.login
-    dir = "${path.module}"
-    conf = jsondecode(file("${path.module}/../proxmox/vm_config_1.json"))
-    repo = "Pod-Shop-App-Configs"
-    owner="ji-podhead"
-  }
-
-
 
  
 
