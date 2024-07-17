0c173391473d3c07824b246070050401349883bb
J.I. /podhead <117015142+ji-podhead@users.noreply.github.com>
2024-07-17 15:33:22 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,8 @@
 
   terraform {
     required_providers {
-        proxmox = {
-        source  = "telmate/proxmox"
-        version = "3.0.1-rc3"
-      }
-      github = {
-        source  = "integrations/github"
-        version = "~> 6.0"
-      }
+
+
     }
   }
   locals {
@@ -18,35 +12,8 @@
     repo = "Pod-Shop-App-Configs"
     owner="ji-podhead"
   }
-  variable "vault_token"{
-    type        = string
-    sensitive = true
-  }
-  variable "vault_role_id"{
-    type        = string
-    sensitive = true
-
-  }
-  variable "vault_role_secret_id"{
-    type        = string
-    sensitive = true
 
 
-  }
-provider "vault" {
-  address = "http://127.0.0.1:8200"
-token = var.vault_token
-#  NOT WORKING!!!
-#   auth_login {
-#    method = "approle"
-#    path = "auth/approle/login"
-#    parameters = {
-#      role_id   = var.vault_role_id
-#      secret_id = var.vault_role_secret_id
-#    }
-#    
-#  }
-#
   skip_child_token = true
   
 }
